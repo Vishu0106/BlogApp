5c0fea3dad1285a0da032dbc8e848e399311ac99
Vishu0106 <kattameedivishnu2003@gmail.com>
2024-03-08 10:58:06 +0530

    implemented AppWrite authentication

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-
+  console.log(imoort.meta.env.VITE_APPWRITE_ENDPOINT)
   return (
     <>
       <h1>Blog App with AppWrite</h1>
