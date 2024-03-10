69cd55a21c88b8b74b0c4ed13fbc7cc1f7bb3e17
Vishu0106 <kattameedivishnu2003@gmail.com>
2024-03-10 12:25:49 +0530

    Add new components , configured TailwindCSS and cofigure redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import { useEffect, useState } from 'react'
+import authService from './appwrite/auth'
+import { login } from './store/authSlice'
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router'
+
 
 function App() {
-  console.log(imoort.meta.env.VITE_APPWRITE_ENDPOINT)
-  return (
-    <>
-      <h1>Blog App with AppWrite</h1>
-    </>
-  )
+  
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+
+  useEffect(() => {
+     authService.getCurrentUser()
+     .then((userData)=>{
+      if(userData) {
+        dispatch(login({status:true, userData}))
+      } else {
+        dispatch(logout())
+      }
+     })
+     .finally(()=>setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          {/* {<Outlet />} */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ): null
 }
 
 export default App
